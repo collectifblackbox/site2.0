473469f869b529a02191ef9afbdc39a4ad51da00
slounis <slounis@xyz-tc.com>
2019-03-22 18:12:35 -0400

    finshed every pages

--- js/scriptport.js
+++ js/scriptport.js
@@ -2,21 +2,22 @@
 $( document ).ready(function() {
 
 	var $grid = $('.grid').imagesLoaded( function() {
-  $grid.masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer'
-  }); 
-});
-
-
+	  	$grid.masonry({
+		    itemSelector: '.grid-item',
+		    percentPosition: true,
+		    columnWidth: '.grid-sizer'
+	  	});
+	 });
+  	var $grid_artistes = $('.grid2').imagesLoaded( function() {
+	  	$grid_artistes.masonry({
+		    itemSelector: '.grid-item2',
+		    percentPosition: true,
+		    columnWidth: '.grid-sizer2'
+	  	});
+	});
 });
 
 $(window).scroll(init);
-
-	
 function init(){
 
-
-				
 }
